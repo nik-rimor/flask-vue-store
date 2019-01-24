4954ca9cc203722befc45cef68f56c0e177ea1ad
nik-rimor <nickkekes@gmail.com>
2019-01-24 18:58:08 +0200

    first flask webapp dockerized setup

---
+++ services/webapp/manage.py
@@ -0,0 +1,14 @@
+# services/webapp/manage.py
+
+
+from flask.cli import FlaskGroup
+
+from project import app
+
+
+cli=FlaskGroup(app)
+
+
+if __name__ == '__main__':
+    cli()
+
---
+++ services/webapp/project/__init__.py
@@ -0,0 +1,27 @@
+# services/webapp/project/__init__.py
+
+
+import os
+
+from flask import Flask, jsonify
+
+
+# instantiate the app
+app = Flask(__name__)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+
+# display selected config settings
+import sys
+print(app.config, file=sys.stderr)
+
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!',
+    })
